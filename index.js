3eb5196082ee8b4ea7678ccbb5ba4938b543cdbd
Dev Ryu <eostmghf01@gmail.com>
2019-08-30 20:14:37 +0900

    2-3

---
+++ index.js
@@ -0,0 +1,6 @@
+// console.log(document);
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! JS";
+title.style.color = "red";
+document.title = "i own you";
+console.dir(document);
