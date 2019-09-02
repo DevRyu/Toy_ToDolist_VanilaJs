b4a572bdd848a9178d851931034ab74de350356b
Dev Ryu <eostmghf01@gmail.com>
2019-09-02 22:06:41 +0900

    2-6

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// console.log(document);
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "rgb(52, 73, 94)";
+
+const OTHER_COLOR = "#7f8c8d";
+
 function handClick() {
-  title.style.color = "blue";
+  const currentColor = title.style.color;
+  console.log(currentColor);
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  }
+  //현재색과 같은 색갈이면 base컬러를 other로 바꿔달라는말
+  else {
+    title.style.color = BASE_COLOR;
+  } //현재색과 다르면 BASE컬러로 해달라는말
 }
 
-title.addEventListener("click", handClick);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handClick);
+}
+init();
