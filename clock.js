c5e1df480a48036ecdac4fd7a3600676be0d9226
Dev Ryu <eostmghf01@gmail.com>
2019-09-03 11:36:22 +0900

    3-2-2 after

--- clock.js
+++ clock.js
@@ -8,8 +8,11 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
   //키보드 숫자 1 옆에있는 ``백틱기호를 사용할거야
+  // 조건 if(?) `참` :(else) 거짓 순으로 되어있는 삼항연산자를 쓸거야
 }
 
 function init() {
