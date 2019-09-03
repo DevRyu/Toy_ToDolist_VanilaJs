00a7df34fcd0315a17783f2d5447aa3faa51d1ef
Dev Ryu <eostmghf01@gmail.com>
2019-09-03 10:30:10 +0900

    2-7-4 toggle

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handClick() {
-  const hasClass = title.classList.contains(CLICKED_CLASS);
-  //contains는 clicked라는 클래스명을 추가적으로 존재하는지 아닌지 알려주지
-  if (hasClass) {
-    title.classList.remove(CLICKED_CLASS);
-    // 처음에는 html에 btn 밖에없으니 지
-  } else {
-    title.classList.add(CLICKED_CLASS);
-    // 클래스가 있으면 클래스를 비워줘 라는거야
-  }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
