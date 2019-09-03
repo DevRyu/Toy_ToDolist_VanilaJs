5305b12eb9c785a9de07d52d577fb07754f5d1f1
Dev Ryu <eostmghf01@gmail.com>
2019-09-03 23:01:45 +0900

    3-3-1 temporary
    not imoprtant

--- greeting.js
+++ greeting.js
@@ -1,6 +1,26 @@
 const form = document.querySelector(".js-form"),
-  input = form.querySelector("input");
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-function init() {}
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    //로컬리스트에 유저가 없을때
+  } else {
+    paintGreeting(currentUser);
+  }
+  //로컬리스트에 유저가 있을떄
+}
+function init() {
+  loadName();
+}
 
 init();
