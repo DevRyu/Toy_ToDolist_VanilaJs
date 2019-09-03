91b0062e93447b7d58e1ff00a3be356a2b640186
Dev Ryu <eostmghf01@gmail.com>
2019-09-03 10:16:15 +0900

    2-7-2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const CLICKED_CLASS = "clicked";
 function handClick() {
   const currentClass = title.className;
   if (currentClass !== CLICKED_CLASS) {
-    title.className = CLICKED_CLASS;
+    title.classList.add(CLICKED_CLASS);
     // 클래스명이 없으면 CLICKED_CLASS = "clicked"으로 클래스만들어주고
   } else {
-    title.className = "";
+    title.className.remove(C);
     // 클래스가 있으면 클래스를 비워줘 라는거야
   }
 }
