0be9228b84915994c509ca9df232efaa3a43b352
Dev Ryu <eostmghf01@gmail.com>
2019-09-06 17:24:44 +0900

    3-8

--- backgr.js
+++ backgr.js
@@ -2,13 +2,21 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 6;
 
+function handleImgLoad() {
+  console.log("f");
+}
+
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `image/${imgNumber + 1}.jpg`;
-  body.appendChild(image);
+  image.classList.add("bgImage");
+  body.prepend(image);
+  //   image.addEventListener("loadend");
+  //약간의 치팅으로 테이블 리스너를 이미지화 하는것 loadend 말그대로 로드엔드후 보여주기!
+
+  //body.appendChild(image);
+  //이미지를 객체로 불러와 저장한후 바디에 추가
 }
-//이미지를 객체로 불러와 저장한후 바디에 추가
-//약간의 치팅으로 테이블 리스너를 이미지화 한는것
 
 function genRandom() {
   const number = Math.floor(Math.random() * IMG_NUMBER);
