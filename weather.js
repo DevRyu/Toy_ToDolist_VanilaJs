7e75b9c4eea6d6b7d33b352f28c011ebc00818bc
Dev Ryu <eostmghf01@gmail.com>
2019-09-07 17:13:06 +0900

    final(withoutbeauty)

--- weather.js
+++ weather.js
@@ -1,10 +1,25 @@
-const API_KEY = "9c6579a5a1cefd4656d3f1b2fc06e843";
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "input-apikey";
 
 const COORDS = "coords";
 
 function getWeather(lat, lng) {
-  fetch(`https//:api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}
-    `);
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+      //서버가 보내는 JSON 데이터가 준비되면
+    })
+    .then(function(json) {
+      const temprature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temprature} @ ${place}`;
+      // 그데이터에 weather DOM에서 온도와 지역을실행시킨다.
+    });
+  // then은 데이터가 우리한데 완전히 넘어온 다음 호출하는 함수
+  // 사용한이유는 fetch가 완료된후 이용되어저야 하기 때문
   // 가져올 데이터 링크만들어가면됨
 }
 
@@ -16,9 +31,9 @@ function handleGeoSuccess(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const coordsObj = {
-    latitude: latitude,
+    latitude,
     // 키와 벨류값이 같게 하려면 그냥 latitude한번만써도됨
-    longitude: longitude
+    longitude
   };
   saveCoords(coordsObj);
   getWeather(latitude, longitude);
